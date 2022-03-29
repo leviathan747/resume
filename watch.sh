2cbbe74ee941e04d7d22fcc4020348053d5051c3
Levi Starrett <levi@roxsoftware.com>
2022-03-29 15:39:12 -0400

    update scripts

--- watch.sh
+++ watch.sh
@@ -2,11 +2,11 @@
 
 STAT_OUTPUT=
 while true; do
-  NEW_STAT_OUTPUT=`stat resume.md`
+  NEW_STAT_OUTPUT=`stat src/resume.md`
   if [[ "$NEW_STAT_OUTPUT" != "$STAT_OUTPUT" ]]; then
     echo "Changes detected. Regenerating..."
     bash generate.sh
-    STAT_OUTPUT=`stat resume.md`
+    STAT_OUTPUT=`stat src/resume.md`
   fi
   sleep 1
 done
